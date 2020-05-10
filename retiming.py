2ac0456161fe737f17500aa61162192cfec48e9a
jmagi98 <jmaginnes@gmail.com>
2020-05-10 08:49:34 -0400

    method for retime calculations based on selection

--- retiming.py
+++ retiming.py
@@ -5,6 +5,49 @@ import maya.mel as mel
 class RetimingTool(object):
     
     @classmethod
+    def retime_keys(cls, retime_val, incremental, move_to_next):
+        range_start, range_end = cls.get_selected_range()
+        start_key = cls.get_start_keyframe_time(range_start)
+        last_key = cls.get_last_keyframe_time()
+        
+        current = start_key
+        current_keyframe_values = [start_key]
+        new_keyframes = [start_key]
+        
+        # iterate starting at first key and going to last
+        while current != last_key:
+            # get the next keyframe
+            next_keyframe = cls.find_keyframe("next", current)
+            if incremental:
+                # get how many frames between keyframes
+                diff = next_keyframe - current
+                
+                # Ensure we are not at the end of the keyframes
+                if current < range_end:
+                    
+                    # calcuate the amount to retime from current key
+                    diff += retime_val
+                    if diff < 1:
+                        diff = 1
+            else:
+                
+                if current < range_end:
+                    # if it is not incremental, we make the difference = to the retime value.
+                    diff = retime_val
+                else:
+                    # Outside selection only moves in releation
+                    diff = next_keyframe - current
+                    
+            # add retime value (diff) to last key in list)
+            new_keyframes.append(new_keyframes[-1] + diff)    
+            current = next_keyframe
+            current_keyframe_values.append(current)
+        print(current_keyframe_values)
+        print(new_keyframes)
+                
+        
+    
+    @classmethod
     def set_current_time(cls, time):
         cmds.currentTime(time)
     
@@ -38,4 +81,5 @@ class RetimingTool(object):
         
         
 if __name__ == '__main__':
-    print(RetimingTool.get_last_keyframe_time())
+    print(RetimingTool.retime_keys(3, False, False))
+    
